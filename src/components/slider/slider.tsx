4ec3933387ec558d2362ca507e2b49a1b002b57d
Alex Marshall <alex.k.marshall83@gmail.com>
2021-10-22 12:03:18 +0100

    add form logic

--- src/components/slider/slider.tsx
+++ src/components/slider/slider.tsx
@@ -1,4 +1,37 @@
-type Props = {}
-export function Slider(props: Props): JSX.Element {
-  return <></>
+import { ChangeEvent, ComponentPropsWithoutRef } from 'react'
+
+type Props<T extends unknown> = {
+  options: T[]
+  value: T
+  onChange: (value: T) => void
+  id?: string
+}
+export function Slider<T>({
+  options,
+  value,
+  onChange,
+  ...props
+}: Props<T>): JSX.Element {
+  const numOfOptions = options.length
+
+  const inputValue = options.findIndex((option) => option === value) + 1 // 1-based index
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const newInputValue = parseInt(event.target.value)
+    const newSelectedOption = options[newInputValue - 1] // convert to zero-based index
+    onChange(newSelectedOption)
+  }
+
+  return (
+    <>
+      <input
+        type="range"
+        min="1"
+        max={numOfOptions}
+        value={inputValue}
+        onChange={handleInputChange}
+        {...props}
+      />
+    </>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,35 @@
 import Head from 'next/head'
 import type { NextPage } from 'next'
+import { Slider } from 'src/components'
+import { useState } from 'react'
+
+const billingPlans = [
+  { traffic: 10_000, price: 8 },
+  { traffic: 50_000, price: 12 },
+  { traffic: 100_000, price: 16 },
+  { traffic: 500_000, price: 24 },
+  { traffic: 1_000_000, price: 32 },
+]
+const discount = 0.25
+type BillingFrequency = 'month' | 'year'
+const priceFormatter = Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+})
 
 const Home: NextPage = () => {
+  const middlePlanIndex = Math.floor(billingPlans.length / 2)
+  const [selectedPlan, setSelectedPlan] = useState(
+    billingPlans[middlePlanIndex]
+  )
+  const [billingFrequency, setSelectedBillingFrequency] =
+    useState<BillingFrequency>('month')
+
+  const priceMultiplier = billingFrequency === 'year' ? 1 - discount : 1
+  const discountPercent = discount * 100
+  const computedPrice = selectedPlan.price * priceMultiplier
+  const formattedPrice = priceFormatter.format(computedPrice)
+
   return (
     <>
       <Head>
@@ -17,21 +45,50 @@ const Home: NextPage = () => {
         <p>Sign up for our 30-day trial.</p>
         <p>No credit card required.</p>
         <form>
-          <output htmlFor="plan-select">100k Pageviews</output>
+          <output htmlFor="plan-select">
+            {selectedPlan.traffic} Pageviews
+          </output>
           <label>
             Select Traffic Level
-            <input type="range" id="plan-select" min={1} max={5} />
+            <Slider
+              id="plan-select"
+              options={billingPlans}
+              value={selectedPlan}
+              onChange={(plan) => setSelectedPlan(plan)}
+            />
           </label>
-          <output htmlFor="plan-select">$16.00 / month</output>
+          <output htmlFor="plan-select billing-frequency">
+            {formattedPrice} per month
+          </output>
           <fieldset id="billing-frequency">
             <legend>Billing Frequency</legend>
             <label>
               Monthly Billing
-              <input type="radio" name="billing-frequency" value="monthly" />
+              <input
+                type="radio"
+                name="billing-frequency"
+                value="month"
+                onChange={(e) =>
+                  setSelectedBillingFrequency(
+                    e.target.value as BillingFrequency
+                  )
+                }
+                checked={billingFrequency === 'month'}
+              />
             </label>
             <label>
-              Yearly Billing -25%
-              <input type="radio" name="billing-frequency" value="yearly" />
+              Yearly Billing -{discountPercent}%
+              <input
+                type="radio"
+                name="billing-frequency"
+                value="year"
+                onChange={(e) =>
+                  setSelectedBillingFrequency(
+                    e.target.value as BillingFrequency
+                  )
+                }
+                checked={billingFrequency === 'year'}
+              />
             </label>
           </fieldset>
           <ul>
