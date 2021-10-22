f46fb85208dcb691397d7b2b78dfe49b35914fb2
Alex Marshall <alex.k.marshall83@gmail.com>
2021-10-22 09:59:29 +0100

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import type { NextPage } from 'next'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <main>
+      <h1>Simple, traffic based pricing</h1>
+    </main>
+  )
+}
+
+export default Home
