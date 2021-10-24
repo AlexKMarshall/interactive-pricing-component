34a81bf12ab71d23e1ecd5102a99cb34acd08107
Alex Marshall <alex.k.marshall83@gmail.com>
2021-10-24 09:38:57 +0100

    add body decoration

--- src/styles/global.css.ts
+++ src/styles/global.css.ts
@@ -8,8 +8,21 @@ globalStyle('html, body', {
 
 globalStyle('body', {
   minHeight: '100vh',
+  position: 'relative',
   backgroundColor: themeTokens.color.neutral[200],
   color: themeTokens.color.neutral[500],
+})
+
+globalStyle('body::before', {
+  content: '',
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  right: 0,
+  height: '60vh',
+  borderBottomLeftRadius: 'clamp(150px, 100px + 10vw, 250px)',
+  backgroundColor: themeTokens.color.neutral[300],
+  zIndex: -1,
 })
 
 globalStyle('h1', {
